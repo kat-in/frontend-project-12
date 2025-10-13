7767931e375ac42ecae8a73d6b995d2ca57ff60e
kat-in <newkatelife@gmail.com>
2025-10-13 23:47:22 +0300

    linter

--- frontend/src/api/socket.js
+++ frontend/src/api/socket.js
@@ -1,20 +1,20 @@
 import { io } from 'socket.io-client'
 
-// const socket = io('http://localhost:5002', {
-//     path: '/socket.io', // путь для проксирования
-//     transports: ['websocket', 'polling'], // обязательно для прокси
-//     autoConnect: false,
-//     // reconnection: true,
-//     // reconnectionAttempts: 5,
-//     // reconnectionDelay: 1000,
-//     // timeout: 5000,
+// const socket = io(process.env.NODE_ENV === 'production' ? window.location.origin : 'http://localhost:5002', {
+//   path: '/socket.io',
+//   transports: ['websocket', 'polling'],
+//   secure: process.env.NODE_ENV === 'production',
+//   withCredentials: true,
 // })
 
-const socket = io(process.env.NODE_ENV === 'production' ? window.location.origin : 'http://localhost:5002', {
-  path: '/socket.io',
-  transports: ['websocket', 'polling'],
-  secure: process.env.NODE_ENV === 'production',
-  withCredentials: true,
-})
+const socket = io(
+  import.meta.env.MODE === 'production' ? window.location.origin : 'http://localhost:5002',
+  {
+    path: '/socket.io',
+    transports: ['websocket', 'polling'],
+    secure: import.meta.env.MODE === 'production',
+    withCredentials: true,
+  }
+)
 
 export default socket
