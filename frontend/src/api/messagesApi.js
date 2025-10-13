1f3d721c4a18cd81164215e8815de4cbb6238b89
kat-in <newkatelife@gmail.com>
2025-10-13 19:13:53 +0300

    Обновление кеша в rtk

--- frontend/src/api/messagesApi.js
+++ frontend/src/api/messagesApi.js
@@ -6,6 +6,7 @@ export const messagesApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: '/api/v1/messages',
     prepareHeaders: (headers, { getState }) => {
+      // const token = getState().auth.token // берем токен из authSlice
       const token = getState().auth.token || localStorage.getItem('token')
       if (token) {
         headers.set('Authorization', `Bearer ${token}`)
@@ -24,21 +25,6 @@ export const messagesApi = createApi({
         method: 'POST',
         body: message,
       }),
-      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
-        // оптимистическое обновление (добавляем сообщение сразу)
-        const patchResult = dispatch(
-          messagesApi.util.updateQueryData('getMessages', undefined, (draft) => {
-              draft.push({ ...arg, id: arg.id ?? `${arg.channelId}-${Date.now()}` })
-          })
-        )
-        try {
-          // если сервер ответил — всё ок
-          await queryFulfilled
-        } catch {
-          // если ошибка — откатываем изменения
-          patchResult.undo()
-        }
-      },
       invalidatesTags: ['Messages'],
     }),
     editMessage: builder.mutation({
